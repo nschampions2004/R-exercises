6b8d338f92ed421ad59ab423e0c8634aa15f2385
Kyle Ligon <kylerligon@gmail.com>
2016-10-02 07:19:36 -0500

    Submission on 10.2
    Exercises from 11.25.15

--- logical_vectors_and_operators_eleven_twentyfive_fifteen.R
+++ logical_vectors_and_operators_eleven_twentyfive_fifteen.R
@@ -25,4 +25,33 @@ data[data$vs & data$am, ]
 #TRUE = 1, FALSE = 0, 0 times anything is 0
 
 #solution to example 8
+data[data$vs & data$am, ]
+
+#solution to example 9
+#| - means or
+#|| - means or but will only evaluate if one is true
+#& - means and
+#$$ - means and but will only evaluate the second if the first is true
+
+#solution to example 10
+#I couldn't get this one...
+data$am[data$am == 0] <- 2
+
+#solution to example 11
+data$vs <- (max(data$vs) + max(data$vs))
+
+#solution to example 12
+#didn't know how to do this one...without == atleast
+data[!(data$vs && data$am) == FALSE, ]
+
+
+
+
+
+
+
+
+
+
+
 
